4ba3450a864632b9be8faa16c5f9ab18a4b3a8a2
Makayden Lofthouse <makayden.lofthouse@gmail.com>
2023-06-19 17:36:56 -0600

    Remove todo; can't be done

--- lib/src/consts.dart
+++ lib/src/consts.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 
-// TODO(mloft74): See if this can be added into a theme, or consider removing it.
 const _listViewBasePaddingValue = 16.0;
 const edgeInsetsForScrollingView = EdgeInsets.fromLTRB(
   _listViewBasePaddingValue,
