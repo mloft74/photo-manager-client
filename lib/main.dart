267fd66e15e4e3bf4305e452e20d924c5b32ee5a
Makayden Lofthouse <makayden.lofthouse@gmail.com>
2023-06-21 19:18:05 -0600

    Make main sync

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:photo_manager_client/src/photo_manager_app.dart';
 
-Future<void> main() async {
-  WidgetsFlutterBinding.ensureInitialized();
+void main() {
   runApp(const ProviderScope(child: PhotoManagerApp()));
 }
