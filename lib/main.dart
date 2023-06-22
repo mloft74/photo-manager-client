badf742c36641f342a572a5db7fea2e2a983f60b
Makayden Lofthouse <makayden.lofthouse@gmail.com>
2023-06-21 19:48:58 -0600

    Add some error handling

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,21 @@
+import 'dart:developer';
+
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:photo_manager_client/src/photo_manager_app.dart';
 
 void main() {
+  // FlutterError.onError can be assigned to if custom error handling is needed.
+
+  PlatformDispatcher.instance.onError = (exception, stackTrace) {
+    log(
+      'Error caught by PlatformDispactcher',
+      error: exception,
+      stackTrace: stackTrace,
+    );
+    return true;
+  };
+
   runApp(const ProviderScope(child: PhotoManagerApp()));
 }
