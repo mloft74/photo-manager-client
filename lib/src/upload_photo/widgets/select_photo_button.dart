f3015cf2848a5889a71c9c5d90932e23eefc1fdb
Makayden Lofthouse <makayden.lofthouse@gmail.com>
2023-06-25 09:36:53 -0600

    Start switching to file picker

--- lib/src/upload_photo/widgets/select_photo_button.dart
+++ lib/src/upload_photo/widgets/select_photo_button.dart
@@ -1,10 +1,8 @@
-import 'dart:async';
+import 'dart:developer';
 
+import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:image_picker/image_picker.dart';
-import 'package:photo_manager_client/src/data_structures/option.dart';
-import 'package:photo_manager_client/src/upload_photo/providers/photo_provider.dart';
 
 class SelectPhotoButton extends ConsumerWidget {
   const SelectPhotoButton({
@@ -14,13 +12,17 @@ class SelectPhotoButton extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     return FilledButton(
-      onPressed: () => unawaited(
-        ref.read(photoProvider.notifier).updateAsync(
-              () async =>
-                  (await ImagePicker().pickImage(source: ImageSource.gallery))
-                      .option,
-            ),
-      ),
+      onPressed: () async {
+        final foo = await FilePicker.platform.getDirectoryPath();
+        log('foo: $foo', name: 'select_photo_button');
+        //   unawaited(
+        //   ref.read(photoProvider.notifier).updateAsync(
+        //         () async =>
+        //             (await ImagePicker().pickImage(source: ImageSource.gallery))
+        //                 .option,
+        //       ),
+        // );
+      },
       child: const Text('Choose image'),
     );
   }
