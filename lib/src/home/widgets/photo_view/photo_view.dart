2071cda14d488ef47e079916422fe0322e2db5c1
Makayden Lofthouse <makayden.lofthouse@gmail.com>
2023-07-29 16:54:14 -0600

    Finish convert provider to async

--- lib/src/home/widgets/photo_view/photo_view.dart
+++ lib/src/home/widgets/photo_view/photo_view.dart
@@ -2,91 +2,91 @@ import 'dart:async';
 
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:photo_manager_client/src/consts.dart';
 import 'package:photo_manager_client/src/data_structures/result.dart';
 import 'package:photo_manager_client/src/home/widgets/photo_view/providers/models/paginated_photos_state.dart';
 import 'package:photo_manager_client/src/home/widgets/photo_view/providers/paginated_photos_provider.dart';
 import 'package:photo_manager_client/src/home/widgets/photo_view/providers/photo_url_provider.dart';
+import 'package:photo_manager_client/src/widgets/async_value_builder.dart';
 
-class NewPhotoView extends HookConsumerWidget {
+class NewPhotoView extends ConsumerWidget {
   const NewPhotoView({super.key});
+
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final providerInitialized = useState(false);
-    if (!providerInitialized.value) {
-      WidgetsBinding.instance.addPostFrameCallback((timeStamp) {
-        unawaited(ref.read(paginatedPhotosProvider.notifier).nextPage());
-        providerInitialized.value = true;
-      });
-    }
-
-    final state = ref.watch(paginatedPhotosProvider);
-    const maxCrossAxisExtent = 256.0;
-    return RefreshIndicator(
-      onRefresh: () async {
-        unawaited(
-          ref.read(paginatedPhotosProvider.notifier).refreshImages(),
-        );
-      },
-      child: CustomScrollView(
-        slivers: [
-          SliverPadding(
-            padding: edgeInsetsForRoutePadding.copyWith(bottom: 0.0),
-            sliver: SliverGrid.builder(
-              itemCount: state.images.length,
-              gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
-                maxCrossAxisExtent: maxCrossAxisExtent,
-                mainAxisSpacing: 8.0,
-                crossAxisSpacing: 8.0,
-              ),
-              itemBuilder: (context, index) {
-                final image = state.images[index];
-                return Consumer(
-                  builder: (context, ref, child) {
-                    final url = ref.watch(
-                      photoUrlProvider(fileName: image.fileName),
-                    );
-                    // TODO(mloft74): make this touchable
-                    return CachedNetworkImage(
-                      imageUrl: url,
-                      progressIndicatorBuilder: (context, url, progress) =>
-                          CircularProgressIndicator(value: progress.progress),
+    return AsyncValueBuilder(
+      asyncValue: ref.watch(paginatedPhotosProvider),
+      builder: (context, state) {
+        const maxCrossAxisExtent = 256.0;
+        return RefreshIndicator(
+          onRefresh: () async {
+            unawaited(
+              ref.read(paginatedPhotosProvider.notifier).refreshImages(),
+            );
+          },
+          child: CustomScrollView(
+            slivers: [
+              SliverPadding(
+                padding: edgeInsetsForRoutePadding.copyWith(bottom: 0.0),
+                sliver: SliverGrid.builder(
+                  itemCount: state.images.length,
+                  gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
+                    maxCrossAxisExtent: maxCrossAxisExtent,
+                    mainAxisSpacing: 8.0,
+                    crossAxisSpacing: 8.0,
+                  ),
+                  itemBuilder: (context, index) {
+                    final image = state.images[index];
+                    return Consumer(
+                      builder: (context, ref, child) {
+                        final url = ref.watch(
+                          photoUrlProvider(fileName: image.fileName),
+                        );
+                        // TODO(mloft74): make this touchable
+                        return CachedNetworkImage(
+                          imageUrl: url,
+                          progressIndicatorBuilder: (context, url, progress) {
+                            return CircularProgressIndicator(
+                              value: progress.progress,
+                            );
+                          },
+                        );
+                      },
                     );
                   },
-                );
-              },
-            ),
-          ),
-          switch (state.loading) {
-            Ok(value: PaginatedPhotosLoadingState.ready) =>
-              const SliverPadding(padding: EdgeInsets.zero),
-            Ok(value: PaginatedPhotosLoadingState.loading) => SliverPadding(
-                padding: edgeInsetsForRoutePadding.copyWith(top: 8.0),
-                sliver: const SliverToBoxAdapter(
-                  child: Center(
-                    child: SizedBox.square(
-                      dimension: maxCrossAxisExtent,
-                      child: CircularProgressIndicator(),
+                ),
+              ),
+              switch (state.loading) {
+                Ok(value: PaginatedPhotosLoadingState.ready) =>
+                  const SliverPadding(padding: EdgeInsets.zero),
+                Ok(value: PaginatedPhotosLoadingState.loading) => SliverPadding(
+                    padding: edgeInsetsForRoutePadding.copyWith(top: 8.0),
+                    sliver: const SliverToBoxAdapter(
+                      child: Center(
+                        child: SizedBox.square(
+                          dimension: maxCrossAxisExtent,
+                          child: CircularProgressIndicator(),
+                        ),
+                      ),
                     ),
                   ),
-                ),
-              ),
-            Err(:final error) => SliverPadding(
-                padding: edgeInsetsForRoutePadding.copyWith(top: 8.0),
-                sliver: SliverToBoxAdapter(
-                  child: Center(
-                    child: Text(error, textAlign: TextAlign.center),
+                Err(:final error) => SliverPadding(
+                    padding: edgeInsetsForRoutePadding.copyWith(top: 8.0),
+                    sliver: SliverToBoxAdapter(
+                      child: Center(
+                        child: Text(error, textAlign: TextAlign.center),
+                      ),
+                    ),
                   ),
-                ),
-              ),
-          },
-          SliverPadding(
-            padding: edgeInsetsForRoutePadding.copyWith(top: 0.0),
-          )
-        ],
-      ),
+              },
+              SliverPadding(
+                padding: edgeInsetsForRoutePadding.copyWith(top: 0.0),
+              )
+            ],
+          ),
+        );
+      },
     );
   }
 }
--- lib/src/home/widgets/photo_view/providers/paginated_photos_provider.dart
+++ lib/src/home/widgets/photo_view/providers/paginated_photos_provider.dart
@@ -6,6 +6,7 @@ import 'package:http/http.dart';
 import 'package:photo_manager_client/src/data_structures/option.dart';
 import 'package:photo_manager_client/src/data_structures/result.dart';
 import 'package:photo_manager_client/src/domain/hosted_image.dart';
+import 'package:photo_manager_client/src/domain/server.dart';
 import 'package:photo_manager_client/src/home/widgets/photo_view/providers/models/paginated_photos_state.dart';
 import 'package:photo_manager_client/src/home/widgets/photo_view/providers/models/photos_page.dart';
 import 'package:photo_manager_client/src/persistence/server/providers/current_server_unchecked_provider.dart';
@@ -19,102 +20,127 @@ class PaginatedPhotos extends _$PaginatedPhotos {
   var _hasNextPage = true;
 
   @override
-  PaginatedPhotosState build() {
-    return const PaginatedPhotosState(
-      loading: Ok(PaginatedPhotosLoadingState.ready),
-      images: [],
+  Future<PaginatedPhotosState> build() async {
+    final result = await _fetchPhotosPage(
+      ref.watch(currentServerUncheckedProvider),
+      _cursor,
+    );
+    return _stateFromResult(
+      result,
+      const PaginatedPhotosState(
+        loading: Ok(PaginatedPhotosLoadingState.loading),
+        images: [],
+      ),
     );
   }
 
   Future<void> refreshImages() async {
     _cursor = const None();
     _hasNextPage = true;
-    state = const PaginatedPhotosState(
-      loading: Ok(PaginatedPhotosLoadingState.ready),
-      images: [],
+    state = const AsyncData(
+      PaginatedPhotosState(
+        loading: Ok(PaginatedPhotosLoadingState.ready),
+        images: [],
+      ),
     );
 
     await nextPage();
   }
 
   Future<void> nextPage() async {
-    final shouldShortCircuitLoading = state.loading
-        .map((value) => value == PaginatedPhotosLoadingState.loading)
-        .unwrapOr(true);
-    if (shouldShortCircuitLoading || !_hasNextPage) {
-      return;
-    }
-    state =
-        state.copyWith(loading: const Ok(PaginatedPhotosLoadingState.loading));
+    if (state case AsyncData(value: final stateData)) {
+      final shouldShortCircuitLoading = stateData.loading
+          .map((value) => value == PaginatedPhotosLoadingState.loading)
+          .unwrapOr(true);
+      if (shouldShortCircuitLoading || !_hasNextPage) {
+        return;
+      }
+      state = AsyncData(
+        stateData.copyWith(
+          loading: const Ok(PaginatedPhotosLoadingState.loading),
+        ),
+      );
 
-    final result = await _fetchPhotosPage();
+      final result = await _fetchPhotosPage(
+        ref.read(currentServerUncheckedProvider),
+        _cursor,
+      );
+      state = AsyncData(_stateFromResult(result, stateData));
+    }
+  }
+
+  PaginatedPhotosState _stateFromResult(
+    Result<PhotosPage, String> result,
+    PaginatedPhotosState stateData,
+  ) {
     switch (result) {
       case Ok(:final value):
         _cursor = value.cursor;
         _hasNextPage = _cursor.isSome;
 
-        state = state.copyWith(
+        return stateData.copyWith(
           loading: const Ok(PaginatedPhotosLoadingState.ready),
-          images: state.images + value.images,
+          images: stateData.images + value.images,
         );
       case Err(:final error):
         log(
           'error fetching page',
-          name: 'PhotoView | _nextPage',
+          name: 'paginatedPhotosProvider | nextPage',
           error: error,
         );
 
         _hasNextPage = false;
 
-        state = state.copyWith(loading: Err(error));
+        return stateData.copyWith(loading: Err(error));
     }
   }
+}
 
-  Future<Result<PhotosPage, String>> _fetchPhotosPage() async {
-    final server = ref.read(currentServerUncheckedProvider);
+Future<Result<PhotosPage, String>> _fetchPhotosPage(
+  Server server,
+  Option<int> after,
+) async {
+  const countParam = 'count=50';
+  final params = after.mapOr(
+    map: (value) => '$countParam&after=$value',
+    or: countParam,
+  );
+  final uri = Uri.parse('${server.uri}/api/image/paginated?$params');
+  log('uri: $uri', name: 'photosPageProvider');
 
-    const countParam = 'count=50';
-    final params = _cursor.mapOr(
-      map: (value) => '$countParam&after=$value',
-      or: countParam,
+  final response = await get(uri);
+  if (response.statusCode != 200) {
+    final reason = response.reasonPhrase ?? 'NO REASON FOUND';
+    final error = 'Reason: $reason, Body: ${response.body}';
+    return Err(error);
+  }
+
+  final body = jsonDecode(response.body);
+  if (body
+      case {
+        'cursor': final int? cursor,
+        'images': final List<dynamic> images,
+      }) {
+    final parsedImages = images
+        .map(
+          (e) => HostedImage(
+            // ignore: avoid_dynamic_calls
+            fileName: e['file_name'] as String,
+            // ignore: avoid_dynamic_calls
+            width: e['width'] as int,
+            // ignore: avoid_dynamic_calls
+            height: e['height'] as int,
+          ),
+        )
+        .toList();
+
+    return Ok(
+      PhotosPage(
+        images: parsedImages,
+        cursor: cursor.option,
+      ),
     );
-    final uri = Uri.parse('${server.uri}/api/image/paginated?$params');
-    log('uri: $uri', name: 'photosPageProvider');
-
-    final response = await get(uri);
-    if (response.statusCode != 200) {
-      final reason = response.reasonPhrase ?? 'NO REASON FOUND';
-      final error = 'Reason: $reason, Body: ${response.body}';
-      return Err(error);
-    }
-
-    final body = jsonDecode(response.body);
-    if (body
-        case {
-          'cursor': final int? cursor,
-          'images': final List<dynamic> images,
-        }) {
-      final parsedImages = images
-          .map(
-            (e) => HostedImage(
-              // ignore: avoid_dynamic_calls
-              fileName: e['file_name'] as String,
-              // ignore: avoid_dynamic_calls
-              width: e['width'] as int,
-              // ignore: avoid_dynamic_calls
-              height: e['height'] as int,
-            ),
-          )
-          .toList();
-
-      return Ok(
-        PhotosPage(
-          images: parsedImages,
-          cursor: cursor.option,
-        ),
-      );
-    } else {
-      return Err('Unknown body: $body');
-    }
+  } else {
+    return Err('Unknown body: $body');
   }
 }
