d3682c97766dc55eae2982cf7920c42ccc04568d
Makayden Lofthouse <makayden.lofthouse@gmail.com>
2023-09-25 17:59:12 -0600

    Fix bug where wrong UI state was showing

--- lib/src/home/widgets/photo_view.dart
+++ lib/src/home/widgets/photo_view.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:photo_manager_client/src/home/pods/models/paginated_photos_state.dart';
 import 'package:photo_manager_client/src/home/pods/paginated_photos_pod.dart';
 import 'package:photo_manager_client/src/home/widgets/photo_view/widgets/photo_view_list.dart';
 import 'package:photo_manager_client/src/home/widgets/photo_view/widgets/photo_view_no_images.dart';
@@ -13,7 +14,10 @@ class PhotoView extends ConsumerWidget {
     return AsyncValueBuilder(
       asyncValue: ref.watch(paginatedPhotosPod),
       builder: (context, state) {
-        if (state.images.isEmpty) {
+        final okAndReady = state.loading.isOkAnd(
+          (value) => value == PaginatedPhotosLoadingState.ready,
+        );
+        if (okAndReady && state.images.isEmpty) {
           return const PhotoViewNoImages();
         } else {
           return PhotoViewList(state: state);
