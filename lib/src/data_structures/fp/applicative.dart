d9642573d748a8763e20e719e815bda2bb030985
Makayden Lofthouse <makayden.lofthouse@gmail.com>
2024-09-01 14:43:45 -0600

    Enable macros

--- lib/src/data_structures/fp/applicative.dart
+++ lib/src/data_structures/fp/applicative.dart
@@ -1,10 +1,16 @@
 // ignore_for_file: one_member_abstracts
 
+import 'dart:async';
+
+import 'package:macros/macros.dart';
+import 'package:photo_manager_client/src/data_structures/fp/functor.dart';
+
 abstract interface class ApplicativePure<TBrand> {
   Applicative<TBrand, TVal> call<TVal>(TVal val);
 }
+typedef ApplicativePureFn<TBrand> = Applicative<TBrand, TVal> Function<TVal>(TVal val);
 
-abstract interface class Applicative<TBrand, TVal> {
+abstract interface class Applicative<TBrand, TVal> implements Functor<TBrand, TVal> {
   /// This is <*> with the arguments reversed. That way, [ApplicativeExtension.applyF] has the arguments the right way around.
   /// Example: [applyR] corresponds to `app <*> this`, but is written in dart like `this.rapply(app)`.
   Applicative<TBrand, TNewVal> applyR<TNewVal>(
@@ -12,8 +18,17 @@ abstract interface class Applicative<TBrand, TVal> {
   );
 }
 
+// Create some annotation for code-genning an extension like this:
 extension ApplicativeExtension<TBrand, TVal, TNewVal>
     on Applicative<TBrand, TNewVal Function(TVal val)> {
   Applicative<TBrand, TNewVal> applyF(Applicative<TBrand, TVal> val) =>
       val.applyR(this);
+}
+
+macro class ApplicativeMacro implements ClassTypesMacro {
+  @override
+  FutureOr<void> buildTypesForClass(ClassDeclaration clazz, ClassTypeBuilder builder) {
+    final name = clazz.identifier.name;
+    builder.declareType('${name}Apply', DeclarationCode.fromString('idk lul'));
+  }
 }
--- lib/src/data_structures/fp/monad.dart
+++ lib/src/data_structures/fp/monad.dart
@@ -1,6 +1,50 @@
 // ignore: one_member_abstracts
-abstract interface class Monad<TBrand, TVal> {
+import 'package:photo_manager_client/src/data_structures/fp/applicative.dart';
+import 'package:photo_manager_client/src/data_structures/fp/functor.dart';
+
+typedef MonadReturn<TBrand> = Monad<TBrand, TVal> Function<TVal>(TVal val);
+
+abstract interface class Monad<TBrand, TVal>
+    implements Applicative<TBrand, TVal> {
   Monad<TBrand, TNewVal> bind<TNewVal>(
     covariant Monad<TBrand, TNewVal> Function(TVal val) fn,
   );
 }
+
+// Create some annotation for code-genning a mixin like this:
+mixin MonadDefaults<TBrand, TVal> on Monad<TBrand, TVal> {
+  Monad<TBrand, T> Function<T>(T val) get $return;
+
+  @override
+  Functor<TBrand, TNewVal> fmap<TNewVal>(TNewVal Function(TVal val) fn) =>
+      bind((val) => $return(fn(val)));
+
+  @override
+  Applicative<TBrand, TNewVal> applyR<TNewVal>(
+    Monad<TBrand, TNewVal Function(TVal val)> other,
+  ) =>
+      bind((val) => other.bind((fn) => $return(fn(val))));
+}
+
+// Example of a way to use MonadDefaults
+/*
+class _X {}
+
+class _Y<TVal> with MonadDefaults<_X, TVal> implements Monad<_X, TVal> {
+  TVal tval;
+  _Y(this.tval);
+
+  @override
+  _Y<T> Function<T>(T val) get $return => _Y.new;
+
+  @override
+  Monad<_X, TNewVal> bind<TNewVal>(_Y<TNewVal> Function(TVal val) fn) =>
+      fn(tval);
+}
+
+void x() {
+  _Y<T> foo<T>(T v) => _Y(v);
+  final MonadReturn<_X> bar = foo;
+  final MonadReturn<_X> x = _Y.new;
+}
+*/
